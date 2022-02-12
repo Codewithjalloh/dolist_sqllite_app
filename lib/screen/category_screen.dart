dc2b936b942dd52c97ad438418f5a7120a7b7fdb
David. J <codewithjalloh@gmail.com>
2022-02-12 18:40:15 +0000

    added a form dialog

--- lib/screen/category_screen.dart
+++ lib/screen/category_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dolist_sqllite_app/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class CategoryScreen extends StatefulWidget {
@@ -8,8 +9,66 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  _showFormDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (param) {
+          return AlertDialog(
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {},
+                child: Text("Cancel"),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text("Save"),
+              ),
+            ],
+            title: const Text("categories form"),
+            content: SingleChildScrollView(
+              child: Column(
+                children: const [
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Write a category",
+                      labelText: "Category",
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Write a decriptions",
+                      labelText: "Description",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: ElevatedButton(
+          onPressed: () => Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => HomeScreen())),
+          child: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+        ),
+        title: Text("Category"),
+      ),
+      body: Center(child: Text("Welcome to the category screen")),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          _showFormDialog(context);
+        },
+      ),
+    );
   }
 }
