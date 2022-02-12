69b19525f0640a8228f054e36821cdacbb9ed720
David. J <codewithjalloh@gmail.com>
2022-02-12 16:45:29 +0000

    created project baseline code

---
+++ android/app/src/main/kotlin/com/example/dolist_sqllite_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dolist_sqllite_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:dolist_sqllite_app/src/app.dart';
+
+void main() => runApp(App());
