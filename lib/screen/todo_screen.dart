5d9dde673020f4473d71583bee049e150fc42778
David. J <codewithjalloh@gmail.com>
2022-02-15 22:30:51 +0000

    added date funtions

--- lib/screen/todo_screen.dart
+++ lib/screen/todo_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dolist_sqllite_app/services/category_service.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TodoScreen extends StatefulWidget {
   @override
@@ -7,9 +8,9 @@ class TodoScreen extends StatefulWidget {
 }
 
 class _TodoScreenState extends State<TodoScreen> {
-  var todoTitleControlller = TextEditingController();
-  var todoDescriptionControlller = TextEditingController();
-  var todoDateControlller = TextEditingController();
+  var _todoTitleControlller = TextEditingController();
+  var _todoDescriptionControlller = TextEditingController();
+  var _todoDateControlller = TextEditingController();
   var _selectedValue;
   var _categories = <DropdownMenuItem>[];
 
@@ -24,12 +25,31 @@ class _TodoScreenState extends State<TodoScreen> {
     var catagories = await _categoryService.readCategories();
     catagories.forEach((category) {
       setState(() {
-        _categories.add(DropdownMenuItem(
-          child: Text(category["name"]),
-          value: category["name"],),);
+        _categories.add(
+          DropdownMenuItem(
+            child: Text(category["name"]),
+            value: category["name"],
+          ),
+        );
       });
     });
+  }
 
+  DateTime _dateTime = DateTime.now();
+
+  _selectedTodoDate(BuildContext context) async {
+    var _pickedDate = await showDatePicker(
+      context: context,
+      initialDate: _dateTime,
+      firstDate: DateTime(2022),
+      lastDate: DateTime(2023),
+    );
+    if (_pickedDate != null) {
+      setState(() {
+        _dateTime = _pickedDate;
+        _todoDateControlller.text = DateFormat("yyyy-MM-dd").format(_pickedDate);
+      });
+    }
   }
 
   @override
@@ -43,23 +63,25 @@ class _TodoScreenState extends State<TodoScreen> {
         child: Column(
           children: [
             TextField(
-              controller: todoTitleControlller,
+              controller: _todoTitleControlller,
               decoration: InputDecoration(
                   labelText: "Title", hintText: "Write to do Title"),
             ),
             TextField(
-              controller: todoDescriptionControlller,
+              controller: _todoDescriptionControlller,
               decoration: InputDecoration(
                   labelText: "Descriptions",
                   hintText: "Write to do Description"),
             ),
             TextField(
-              controller: todoDateControlller,
+              controller: _todoDateControlller,
               decoration: InputDecoration(
                   labelText: "Date",
                   hintText: "Pick a Date",
                   prefixIcon: InkWell(
-                    onTap: () {},
+                    onTap: () {
+                      _selectedTodoDate(context);
+                    },
                     child: Icon(Icons.calendar_today),
                   )),
             ),
